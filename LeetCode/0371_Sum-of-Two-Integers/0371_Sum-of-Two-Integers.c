80550c230ac3a45f58d21519d5276d713b96d876
Ken Jin <kenjin@gmail.com>
2019-05-16 00:34:22 +0800

    Change a clear recursive solution

--- LeetCode/0371_Sum-of-Two-Integers/0371_Sum-of-Two-Integers.c
+++ LeetCode/0371_Sum-of-Two-Integers/0371_Sum-of-Two-Integers.c
@@ -14,24 +14,20 @@ Output: 1
 
  */
 
+/*
+ * 011     110     0100     01000    "1000"
+ * 101 => 0010 => 00100 => 000000 =>     0
+ *
+ * getSum(3,5) => getSum(6,2) => getSum(4,4) => getSum(8,0) => Ans=8
+ */
 int getSum(int a, int b)
 {
-	unsigned int base = 0x1;
-	unsigned int carry = 0;
-	unsigned int sum = 0;
-	for (int i = 0; i < 32; i++)
+	if (b == 0)
 	{
-		unsigned int tmpA = (base << i) & (unsigned int)a;
-		unsigned int tmpB = (base << i) & (unsigned int)b;
-		sum  += (tmpA ^ tmpB) ^ (carry << i);
-		if ((tmpA != 0 && (tmpA ^ tmpB) == 0) /* tmpA and tmpB are > 0 */ || 
-				((tmpA ^ tmpB) > 0 && carry == 1))
-		{
-			carry = 1;
-		} else
-		{
-			carry = 0;
-		}
+		return a;
 	}
-	return sum;
+	unsigned int sum = a ^ b;
+	/* &0x7ffffffff: Clear most significant bit to avoid INT_MIN left shift case */
+	unsigned int carry = (a & b & 0x7ffffffff) << 1;
+	return getSum(sum, carry);
 }
