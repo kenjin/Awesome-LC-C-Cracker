f483d725c2848ec3a619082c51ce917f59357377
Ken Jin <kenjcfar@gmail.com>
2019-05-15 20:42:08 +0800

    Enhance the greedy algorithm

--- LeetCode/0277_Find-the-Celebrity/0277_Find-the-Celebrity.c
+++ LeetCode/0277_Find-the-Celebrity/0277_Find-the-Celebrity.c
@@ -9,52 +9,21 @@ bool knows(int a, int b);
 
 int findCelebrity(int n) 
 {
-	int record[n];
-	memset(record, 0, sizeof(int)*n);
-
-	for (int j = 0; j < n; j++)
+	int ret = 0;
+	for (int i = 0; i < n; i++)
 	{
-		/* I have known somebody */
-		if (record[j] == -1)
+		/* Find the only candidate */
+		ret = knows(i, ret) ? ret : i;
+	}
+	/* Verify candidate */
+	for (int i = 0; i < n; i++)
+	{
+		if (ret != i &&
+				(knows(ret, i) || !knows(i, ret))
+		   )
 		{
-			continue;
-		}
-		for (int i = 0; i < n; i++)
-		{
-			if (i == j)
-			{
-				continue;
-			}
-
-			if (knows(i, j))
-			{
-				record[j] += 1;
-				record[i] = -1;
-			}
-			/* Check that if I know somebody */
-			if (record[j] == n-1)
-			{
-				int nobody = 0;
-				for (int x = 0; x < n; x++)
-				{
-					if (x == j)
-					{
-						continue;
-					}
-					if (knows(j, x))
-					{
-						nobody = 1;
-						break;
-					}
-				}
-				if (!nobody)
-				{
-					return j;
-				}
-			}
-
+			return -1;
 		}
 	}
-
-	return -1;
+	return ret;
 }
