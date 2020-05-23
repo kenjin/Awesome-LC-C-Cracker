624f66925f3fdd88ba4cdea11b740f95378eacdb
ken.jin <kenpc.jin@moxa.com>
2020-05-23 23:01:44 +0800

    Refactor #986 array question

--- LeetCode/0986_interval-list-intersections/0986_interval-list-intersections.c
+++ LeetCode/0986_interval-list-intersections/0986_interval-list-intersections.c
@@ -1,72 +1,51 @@
-bool isIntersection(int *dst, int *src)
+#define MIN(a, b) (a < b ? a : b);
+#define MAX(a, b) (a > b ? a : b);
+
+static inline int **add_result(int **ret,
+                               int *returnSize,
+                               int **returnColumnSizes,
+                               int x,
+                               int y)
 {
-	if ((dst[0] <= src[0] && src[0] <= dst[1]) ||
-			(dst[0] <= src[1] && src[1] <= dst[1]))
-	{
-		return true;
-	}
-	return false;
+    (*returnColumnSizes)[*returnSize] = 2;
+    ret[*returnSize] = malloc(sizeof(int) * 2);
+    ret[*returnSize][0] = x;
+    ret[*returnSize][1] = y;
+    *returnSize += 1;
+    return ret;
 }
 
 /**
  * Return an array of arrays of size *returnSize.
  * The sizes of the arrays are returned as *returnColumnSizes array.
- * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ * Note: Both returned array and *columnSizes array must be malloced, assume
+ * caller calls free().
  */
-int** intervalIntersection(int** A, int ASize, int* AColSize, int** B, int BSize, int* BColSize, int* returnSize, int** returnColumnSizes)
+int **intervalIntersection(int **A,
+                           int ASize,
+                           int *AColSize,
+                           int **B,
+                           int BSize,
+                           int *BColSize,
+                           int *returnSize,
+                           int **returnColumnSizes)
 {
-	int memSizeUnit = ASize+BSize;
-	int **ret = malloc(sizeof(int *)*memSizeUnit);
-	*returnColumnSizes = malloc(sizeof(int)*memSizeUnit);
+    int **ret = malloc(sizeof(int *) * (ASize + BSize));
+    *returnSize = 0;
+    *returnColumnSizes = malloc(sizeof(int) * (ASize + BSize));
 
-	int idxA = 0, idxB = 0, retCtr = 0;
-	while (idxA < ASize && idxB < BSize)
-	{
-		// Find intersection
-		if (isIntersection(A[idxA], B[idxB]) || isIntersection(B[idxB], A[idxA]))
-		{
-			// Add new intersection
-			ret[retCtr] = malloc(sizeof(int)*2);
-			(*returnColumnSizes)[retCtr] = 2;
-			ret[retCtr][0] = (A[idxA][0] > B[idxB][0] ? A[idxA][0] : B[idxB][0]);
-			ret[retCtr][1] = (A[idxA][1] < B[idxB][1] ? A[idxA][1] : B[idxB][1]);
+    int aidx = 0, bidx = 0;
+    while (aidx < ASize && bidx < BSize) {
+        int lb = MAX(A[aidx][0], B[bidx][0]);
+        int ub = MIN(A[aidx][1], B[bidx][1]);
+        if (lb <= ub)
+            ret = add_result(ret, returnSize, returnColumnSizes, lb, ub);
 
-			// Update input
-			if (ret[retCtr][1]+1 <= A[idxA][1])
-			{
-				A[idxA][0] = ret[retCtr][1]+1;
-			} else
-			{
-				idxA++;
-			}
-			if (ret[retCtr][1]+1 <= B[idxB][1])
-			{
-				B[idxB][0] = ret[retCtr][1]+1;
-			} else
-			{
-				idxB++;
-			}
+        if (A[aidx][1] < B[bidx][1])
+            aidx++;
+        else
+            bidx++;
+    }
 
-			retCtr++;
-			if (retCtr+1 % memSizeUnit == 0)
-			{
-				ret = realloc(ret, sizeof(int *)*(retCtr+memSizeUnit));
-				(*returnColumnSizes) = realloc(*returnColumnSizes, sizeof(int)*(retCtr+memSizeUnit));
-			}            
-		} else
-		{
-			// If no intersection in this round, choose the smaller one to increase the index            
-			if (A[idxA][1] <= B[idxB][0])
-			{
-				idxA++;
-			} else
-			{
-				idxB++;
-			}
-		}
-	}
-
-	*returnSize = retCtr;
-	return ret;
+    return ret;
 }
-
