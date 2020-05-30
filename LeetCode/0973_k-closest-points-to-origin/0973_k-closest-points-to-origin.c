7f5d5b898f5f2dd01ab6de663ef8f821e394e026
ken.jin <kenpc.jin@moxa.com>
2020-05-30 23:27:10 +0800

    Refactor #973 via specific qsort
    qsort only do recursion for K size smaller part

--- LeetCode/0973_k-closest-points-to-origin/0973_k-closest-points-to-origin.c
+++ LeetCode/0973_k-closest-points-to-origin/0973_k-closest-points-to-origin.c
@@ -1,42 +1,76 @@
-int compare(const void *a, const void *b)
-{
-	const int *n1 = *(int **)a;
-	const int *n2 = *(int **)b;
 
-	return (n1[1] - n2[1]);
+typedef struct __data {
+    int idx;  /* points index */
+    int dist; /* euclidean distance */
+} DATA;
+
+static inline void swap(DATA *d1, DATA *d2)
+{
+    DATA tmp;
+    tmp.idx = d1->idx;
+    tmp.dist = d1->dist;
+    d1->idx = d2->idx;
+    d1->dist = d2->dist;
+    d2->idx = tmp.idx;
+    d2->dist = tmp.dist;
 }
+
+static void spec_qsort(DATA *d, int dsize, int K)
+{
+    if (dsize <= 1 || K <= 0)
+        return;
+
+    /* partition */
+    int pivot = d[dsize - 1].dist;
+    int sidx = -1, lidx;
+    for (lidx = 0; lidx < dsize - 1; lidx++) {
+        if (d[lidx].dist < pivot) {
+            sidx++;
+            swap(&d[sidx], &d[lidx]);
+        }
+    }
+    sidx++;
+    swap(&d[sidx], &d[dsize - 1]);
+
+    /* recursive sort for left-half and right-half */
+    spec_qsort(d, sidx, K);
+    if (sidx < K)
+        spec_qsort(&(d[sidx + 1]), dsize - 1 - sidx, K - sidx);
+}
+
 /**
  * Return an array of arrays of size *returnSize.
  * The sizes of the arrays are returned as *returnColumnSizes array.
- * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ * Note: Both returned array and *columnSizes array must be malloced, assume
+ * caller calls free().
  */
-int** kClosest(int** points, int pointsSize, int* pointsColSize, int K, int* returnSize, int** returnColumnSizes)
+int **kClosest(int **points,
+               int pointsSize,
+               int *pointsColSize,
+               int K,
+               int *returnSize,
+               int **returnColumnSizes)
 {
-	// data[X][0]: points index
-	// data[X][1]: euclidean distance
-	int **data = calloc(pointsSize, sizeof(int *));
-	for (int i = 0; i < pointsSize; i++)
-	{
-		data[i] = malloc(sizeof(int)*2);
-		data[i][0] = i;
-		data[i][1] = (points[i][0])*(points[i][0]) + (points[i][1])*(points[i][1]);
-	}
-	// sort via the distance
-	qsort(data, pointsSize, sizeof(int *), compare);
+    DATA *d = malloc(pointsSize * sizeof(DATA));
+    for (int i = 0; i < pointsSize; i++) {
+        d[i].idx = i;
+        d[i].dist =
+            (points[i][0]) * (points[i][0]) + (points[i][1]) * (points[i][1]);
+    }
 
-	*returnSize = K;
-	int **ret = malloc(sizeof(int*)*K);
-	*returnColumnSizes = malloc(sizeof(int)*K);
-	for (int i = 0; i < K; i++)
-	{
-		(*returnColumnSizes)[i] = 2;
-		ret[i] = malloc(sizeof(int)*2);
-		int cur_idx = data[i][0];
-		ret[i][0] = points[cur_idx][0];
-		ret[i][1] = points[cur_idx][1];
-		free(data[i]);
-	}
+    spec_qsort(d, pointsSize, K);
 
-	free(data);
-	return ret;
+    *returnSize = K;
+    int **ret = malloc(sizeof(int *) * K);
+    *returnColumnSizes = malloc(sizeof(int) * K);
+    for (int i = 0; i < K; i++) {
+        (*returnColumnSizes)[i] = 2;
+        ret[i] = malloc(sizeof(int) * 2);
+        int cur_idx = d[i].idx;
+        ret[i][0] = points[cur_idx][0];
+        ret[i][1] = points[cur_idx][1];
+    }
+
+    free(d);
+    return ret;
 }
