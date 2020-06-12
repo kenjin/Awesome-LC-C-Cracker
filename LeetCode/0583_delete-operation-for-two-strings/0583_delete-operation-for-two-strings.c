13d21a1c180cfdad4562df23e94ac38aa55ed98d
ken.jin <kenpc.jin@moxa.com>
2020-06-12 09:55:01 +0800

    Solve #593 via LCS concept

---
+++ LeetCode/0583_delete-operation-for-two-strings/0583_delete-operation-for-two-strings.c
@@ -0,0 +1,34 @@
+
+#define MAX(a, b) (a > b ? a : b)
+
+int longestCommonSubsequence(char *text1, char *text2)
+{
+    int len1 = strlen(text1);
+    int len2 = strlen(text2);
+    int **dp = malloc(sizeof(int *) * (len1 + 1));
+    for (int i = 0; i <= len1; i++)
+        dp[i] = calloc(len2 + 1, sizeof(int));
+
+    for (int i = 1; i <= len1; i++) {
+        for (int j = 1; j <= len2; j++) {
+            if (text1[i - 1] == text2[j - 1])
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            else
+                dp[i][j] = MAX(dp[i][j - 1], dp[i - 1][j]);
+        }
+    }
+
+    int ret = dp[len1][len2];
+    for (int i = 0; i <= len1; i++)
+        free(dp[i]);
+    free(dp);
+
+    return ret;
+}
+
+int minDistance(char *word1, char *word2)
+{
+    int lcs = longestCommonSubsequence(word1, word2);
+
+    return strlen(word1) + strlen(word2) - 2 * lcs;
+}
