d99ef1e71913a058640f438477dc7ba04c5e33c8
ken.jin <kenpc.jin@moxa.com>
2020-06-18 11:47:53 +0800

    Refactor #6

--- LeetCode/0006_ZigZag-Conversion/0006_ZigZag-Conversion.c
+++ LeetCode/0006_ZigZag-Conversion/0006_ZigZag-Conversion.c
@@ -1,34 +1,25 @@
 
-char * convert(char * s, int numRows)
+char *convert(char *s, int numRows)
 {
-	if (numRows == 1)
-	{
-		return s;
-	}
+    if (numRows == 1)
+        return s;
 
-	int base = numRows+(numRows-2);
-	int len = strlen(s);
-	char *ret = calloc(len+1, sizeof(char));
-	int ctr = 0;
-	int x, y;
-	for (int i = 0; i < numRows; i++)
-	{
-		x = i;
-		y = base-i;
-		while (x < len)
-		{
-			ret[ctr] = s[x];
-			x += base;
-			ctr++;
-			if (i > 0 && i < numRows-1 && y < len)
-			{
-				ret[ctr] = s[y];
-				y += base;
-				ctr++;
-			}
-		}
-	}
+    int base = (numRows << 1) - 2;
+    int slen = strlen(s);
+    int ret_cnt = 0;
+    char *ret = calloc(slen + 1, sizeof(char));
+    for (int i = 0; i < numRows; i++) {
+        int idx = i;
+        int idx_slash = base - i;
+        while (idx < slen) {
+            ret[ret_cnt++] = s[idx];
+            idx += base;
+            if (i > 0 && i < (numRows - 1) && idx_slash < slen) {
+                ret[ret_cnt++] = s[idx_slash];
+                idx_slash += base;
+            }
+        }
+    }
 
-	return ret;
+    return ret;
 }
-
