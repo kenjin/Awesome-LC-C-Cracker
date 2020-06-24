04a3a96281bf7270078c27243f0fe58551f0ba88
ken.jin <kenpc.jin@moxa.com>
2020-06-24 16:25:01 +0800

    Rewrite #1 and change indent format

--- LeetCode/0001_two-sum/0001_two-sum.c
+++ LeetCode/0001_two-sum/0001_two-sum.c
@@ -1,96 +1,85 @@
-typedef struct hashT HASH;
 
-struct hashInfo
-{
-	int idx;
-	int val;   
-};
+typedef struct __hinfo {
+    int idx;
+    int val;
+} HINFO;
 
-struct hashT
-{
-	int size;
-	int mod;
-	struct hashInfo **arr;
-};
+typedef struct __hash_table {
+    int size;
+    int mod;
+    HINFO **arr;
+} HASH;
 
-int hash(HASH *obj, int key) 
+int hash(HASH *obj, int key)
 {
-	int r = key % obj->mod;
-	return r < 0 ? r + obj->size : r;
+    int r = key % obj->size;
+    return r < 0 ? r + obj->size : r;
 }
 
-HASH* createHash(int size)
+HASH *hash_create(int size)
 {
-	HASH *obj = malloc(sizeof(HASH));
-	obj->size = size;
-	obj->mod = size;
-	obj->arr = malloc(sizeof(struct hashInfo)*(size));
-	for (int x = 0; x < size; x++)
-	{
-		struct hashInfo *newInfo = malloc(sizeof(struct hashInfo));
-		obj->arr[x] = newInfo;
-		newInfo->idx = -1;
-	}
-	return obj;
+    HASH *obj = malloc(sizeof(HASH));
+    obj->size = size;
+    obj->arr = malloc(sizeof(HINFO) * (size));
+    for (int x = 0; x < size; x++) {
+        HINFO *info = malloc(sizeof(HINFO));
+        obj->arr[x] = info;
+        info->idx = -1;
+    }
+    return obj;
 }
 
-void addHash(HASH *ht, int key, int idx) 
+void hash_add(HASH *ht, int key, int idx)
 {
-	int index = hash(ht, key);
-	while (ht->arr[index]->idx != -1) 
-	{
-		index++;
-		index %= ht->size;
-	}
-	ht->arr[index]->idx = idx;
-	ht->arr[index]->val = key;
+    int index = hash(ht, key);
+    while (ht->arr[index]->idx != -1) {
+        index++;
+        index %= ht->size;
+    }
+    ht->arr[index]->idx = idx;
+    ht->arr[index]->val = key;
 }
 
-int findHash(HASH *ht, int target) 
+int hash_find(HASH *ht, int target)
 {
-	int index = hash(ht, target);
-	while (ht->arr[index]->idx != -1) 
-	{
-		if (ht->arr[index]->val == target) 
-		{
-			return ht->arr[index]->idx;
-		}
-		index++;
-		index %= ht->size;
-	}
-	return -1;
+    int index = hash(ht, target);
+    while (ht->arr[index]->idx != -1) {
+        if (ht->arr[index]->val == target)
+            return ht->arr[index]->idx;
+
+        index++;
+        index %= ht->size;
+    }
+    return -1;
 }
 
-void releaseHash(HASH *ht)
+void hash_release(HASH *ht)
 {
-	for (int i = 0; i < ht->size; i++)
-	{
-		free(ht->arr[i]);
-	}
-	free(ht->arr);
-	free(ht);
+    for (int i = 0; i < ht->size; i++)
+        free(ht->arr[i]);
+
+    free(ht->arr);
+    free(ht);
 }
 
-int* twoSum(int* nums, int numsSize, int target, int *returnSize) 
+int *twoSum(int *nums, int numsSize, int target, int *returnSize)
 {
-	int *indices = NULL;
-	*returnSize = 0;
-	HASH *ht = createHash(numsSize);
-	for (int i = 0; i < numsSize; i++) 
-	{
-		int complements = target - nums[i];
-		int index = findHash(ht, complements);
-		if (index >= 0) 
-		{
-			indices = (int *)malloc(sizeof(int) * 2);
-			indices[0] = index;
-			indices[1] = i;
-			*returnSize = 2;
-			break;
-		}
-		addHash(ht, nums[i], i);
-	}
+    int *indices = NULL;
+    *returnSize = 0;
+    HASH *ht = hash_create(numsSize);
+    for (int i = 0; i < numsSize; i++) {
+        int complements = target - nums[i];
+        int index = hash_find(ht, complements);
+        if (index >= 0) {
+            indices = (int *) malloc(sizeof(int) * 2);
+            indices[0] = index;
+            indices[1] = i;
+            *returnSize = 2;
+            break;
+        }
+        hash_add(ht, nums[i], i);
+    }
 
-	releaseHash(ht);
-	return indices;
+    hash_release(ht);
+    return indices;
 }
