c6be11d3de5b700afffdcb235b481961f0dcd144
ken.jin <kenpc.jin@moxa.com>
2020-06-24 18:10:23 +0800

    Rewrite #96

--- LeetCode/0096_Unique-Binary-Search-Trees/0096_Unique-Binary-Search-Trees.c
+++ LeetCode/0096_Unique-Binary-Search-Trees/0096_Unique-Binary-Search-Trees.c
@@ -11,21 +11,17 @@ store values 1 ... n?
 int numTrees(int n)
 {
     if (n <= 1)
-    {
         return 1;
-    }
-    
+
     int ret = 0, midCtr = 0;
-    for (int i = 0; i < n/2 ; i++)
-    {
-        ret += (numTrees(i)*numTrees(n-i-1));
-    }
-    if (n % 2 != 0) /* odd case, handle root in the middle */
-    {
-        int halfCtr = numTrees((n+1)/2 - 1); /* Calculate half ctr */
-        midCtr = halfCtr*halfCtr; /* leftCtr * rightCtr */
+    for (int i = 0; i < n / 2; i++)
+        ret += (numTrees(i) * numTrees(n - i - 1));
+
+    if (n % 2 != 0) {
+        /* odd case, handle root in the middle */
+        int halfCtr = numTrees((n + 1) / 2 - 1); /* Calculate half ctr */
+        midCtr = halfCtr * halfCtr;              /* leftCtr * rightCtr */
     }
 
-    return (ret*2 + midCtr);
+    return (ret * 2 + midCtr);
 }
-
