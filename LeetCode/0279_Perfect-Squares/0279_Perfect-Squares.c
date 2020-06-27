fd362a03d3b670a92809581b7439acc057cd1163
ken.jin <kenpc.jin@moxa.com>
2020-06-27 23:59:24 +0800

    Rewrite #279

--- LeetCode/0279_Perfect-Squares/0279_Perfect-Squares.c
+++ LeetCode/0279_Perfect-Squares/0279_Perfect-Squares.c
@@ -1,9 +1,10 @@
 /**
-279. Perfect Squares [Medium]
+  279. Perfect Squares [Medium]
 
-Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
+  Given a positive integer n, find the least number of perfect square numbers
+(for example, 1, 4, 9, 16, ...) which sum to n.
 
-Example 1:
+  Example 1:
 
 Input: n = 12
 Output: 3
@@ -17,32 +18,29 @@ Explanation: 13 = 4 + 9.
 
  */
 
-int numSquares(int n){
+
+int numSquares(int n)
+{
+    if (n <= 1)
+        return n;
 
     /* dp[i]: recored the least number of perfect square numbers sum to i */
-    int dp[n+1];
-
+    int *dp = malloc(sizeof(int) * (n + 1));
     dp[0] = 0;
-    dp[1] = 1;
-
-    for (int i = 1; i <= n; i++)
-    {
-        int num = 1;
-        int num_square = num*num;
-        int min = i;
-        while (num_square <= i)
-        {
-            /* if i equal to num_square => dp[0], it means that only require one square number */
-            int tmp = 1 + dp[i-num_square];
+    for (int i = 1; i <= n; i++) {
+        int num_square = 1, min = i;
+        for (int num = 1; num_square <= i; num++) {
+            /* if i equal to num_square => dp[0], it means that only require one
+             * square number */
+            int tmp = 1 + dp[i - num_square];
             if (tmp < min)
-            {
                 min = tmp;
-            }
-            num++;
             num_square = num * num;
         }
         dp[i] = min;
     }
 
-    return dp[n];
+    int ret = dp[n];
+    free(dp);
+    return ret;
 }
