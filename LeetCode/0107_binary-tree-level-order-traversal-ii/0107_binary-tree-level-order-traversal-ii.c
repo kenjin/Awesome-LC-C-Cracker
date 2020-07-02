8aafe227aa2c61660cf5f82aa230c08b71f0b28e
ken.jin <kenpc.jin@moxa.com>
2020-07-02 17:49:27 +0800

    Adjust #107 indent

--- LeetCode/0107_binary-tree-level-order-traversal-ii/0107_binary-tree-level-order-traversal-ii.c
+++ LeetCode/0107_binary-tree-level-order-traversal-ii/0107_binary-tree-level-order-traversal-ii.c
@@ -7,53 +7,54 @@
  * };
  */
 
-void lvOrderTraversalHelper(struct TreeNode* root, int **ret, int* retSize, int** retColSize, int level)
+void lvorder_traverse(struct TreeNode *root,
+                      int **ret,
+                      int *ret_sz,
+                      int **ret_col_sz,
+                      int level)
 {
-	if (root == NULL)
-	{
-		return;
-	}
+    if (root == NULL)
+        return;
 
-	lvOrderTraversalHelper(root->left, ret, retSize, retColSize, level-1/* bottom-up */);
-	lvOrderTraversalHelper(root->right, ret, retSize, retColSize, level-1/* bottom-up */);
+    lvorder_traverse(root->left, ret, ret_sz, ret_col_sz,
+                     level - 1 /* bottom-up */);
+    lvorder_traverse(root->right, ret, ret_sz, ret_col_sz,
+                     level - 1 /* bottom-up */);
 
-	ret[level] = realloc(ret[level], sizeof(int)*((*retColSize)[level]+1));
-	ret[level][(*retColSize)[level]] = root->val;
-	(*retColSize)[level] += 1;
-
+    ret[level] = realloc(ret[level], sizeof(int) * ((*ret_col_sz)[level] + 1));
+    ret[level][(*ret_col_sz)[level]] = root->val;
+    (*ret_col_sz)[level] += 1;
 }
 
-int getHeight(struct TreeNode *root)
+int get_height(struct TreeNode *root)
 {
-	if (root == NULL)
-	{
-		return 0;
-	}
+    if (!root)
+        return 0;
 
-	int l = getHeight(root->left);
-	int r = getHeight(root->right);    
-	return ( l > r ? l+1 : r+1);
+    int l = get_height(root->left);
+    int r = get_height(root->right);
+    return (l > r ? l + 1 : r + 1);
 }
 
 /**
  * Return an array of arrays of size *returnSize.
  * The sizes of the arrays are returned as *returnColumnSizes array.
- * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ * Note: Both returned array and *columnSizes array must be malloced, assume
+ * caller calls free().
  */
-int** levelOrderBottom(struct TreeNode* root, int* returnSize, int** returnColumnSizes)
+int **levelOrderBottom(struct TreeNode *root,
+                       int *returnSize,
+                       int **returnColumnSizes)
 {
-	int maxHeight = getHeight(root);
-	int **ret = malloc(sizeof(int *)*maxHeight);
-	for (int i = 0; i < maxHeight; i++)
-	{
-		ret[i] = malloc(sizeof(int));
-	}
+    int height = get_height(root);
+    int **ret = malloc(sizeof(int *) * height);
+    for (int i = 0; i < height; i++)
+        ret[i] = malloc(sizeof(int));
 
-	*returnColumnSizes = calloc(maxHeight, sizeof(int));
-	*returnSize = maxHeight;
-	lvOrderTraversalHelper(root, ret, returnSize, returnColumnSizes, maxHeight-1 /* bottom-up */);
+    *returnColumnSizes = calloc(height, sizeof(int));
+    *returnSize = height;
+    lvorder_traverse(root, ret, returnSize, returnColumnSizes,
+                     height - 1 /* bottom-up */);
 
-	return ret;
+    return ret;
 }
-
-
