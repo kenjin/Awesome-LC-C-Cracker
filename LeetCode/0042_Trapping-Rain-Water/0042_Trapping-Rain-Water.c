d0f29fbaec4c3287bd60f3f784583fb23bdf3328
ken.jin <kenpc.jin@moxa.com>
2020-07-03 10:42:04 +0800

    Rewrite #42 and adjust indent

--- LeetCode/0042_Trapping-Rain-Water/0042_Trapping-Rain-Water.c
+++ LeetCode/0042_Trapping-Rain-Water/0042_Trapping-Rain-Water.c
@@ -1,98 +1,86 @@
-typedef struct
-{
-	int size;
-	int cur;
-	int *arr;
+typedef struct {
+    int size;
+    int cur;
+    int *arr;
 } STACK;
 
-STACK* stackCreate(int size) 
+STACK *stack_create(int size)
 {
-	STACK *obj = malloc(sizeof(STACK));
-	obj->size = size;
-	obj->cur = -1;
-	obj->arr = malloc(sizeof(int)*size);
-	return obj;
+    STACK *obj = malloc(sizeof(STACK));
+    obj->size = size;
+    obj->cur = -1;
+    obj->arr = malloc(sizeof(int) * size);
+    return obj;
 }
 
-bool stackIsEmpty(STACK *obj)
+static inline bool stack_is_empty(STACK *obj)
 {
-	return (obj->cur == -1 ? true : false);
+    return (obj->cur == -1 ? true : false);
 }
 
-void stackPush(STACK* obj, int idx)
+static inline void stack_push(STACK *obj, int idx)
 {
-	obj->cur += 1;
-	obj->arr[obj->cur] = idx;
+    obj->cur += 1;
+    obj->arr[obj->cur] = idx;
 }
 
-int stackPop(STACK* obj)
+static int stack_pop(STACK *obj)
 {
-	if (stackIsEmpty(obj))
-	{
-		printf("WARNING: empty stack!\n");
-		return;
-	}
+    if (stack_is_empty(obj)) {
+        printf("WARNING: empty stack!\n");
+        return;
+    }
 
-	obj->cur -= 1;
-	return obj->arr[obj->cur+1];
+    obj->cur -= 1;
+    return obj->arr[obj->cur + 1];
 }
 
-int stackTop(STACK* obj)
+static inline int stack_top(STACK *obj)
 {
-	if (stackIsEmpty(obj))
-	{
-		return -1;
-	}
-	return obj->arr[obj->cur];
+    if (stack_is_empty(obj))
+        return -1;
+    return obj->arr[obj->cur];
 }
 
-void stackFree(STACK* obj) 
+static inline void stackFree(STACK *obj)
 {
-	free(obj->arr);
-	free(obj);
+    free(obj->arr);
+    free(obj);
 }
 
-int trap(int* height, int heightSize)
+int trap(int *height, int height_sz)
 {
-	STACK *s = stackCreate(heightSize+1);
+    STACK *s = stack_create(height_sz + 1);
 
-	int ret = 0;
-	for (int i = 0; i < heightSize; i++)
-	{
-		if (stackIsEmpty(s) && height[i] == 0)
-		{
-			continue;
-		}
-		if (!stackIsEmpty(s))
-		{
-			int top = height[stackTop(s)];            
-			if (height[i] > top) 
-			{
-				int maxHeight = height[i]; 
-				int lowValleyIdx = stackPop(s);
-				while (!stackIsEmpty(s))
-				{
-					int chkIdx = stackPop(s);
-					int diff = (maxHeight > height[chkIdx]) ? height[chkIdx] - height[lowValleyIdx]
-						: maxHeight - height[lowValleyIdx];
+    int ret = 0;
+    for (int i = 0; i < height_sz; i++) {
+        if (stack_is_empty(s) && height[i] == 0)
+            continue;
 
-					if (diff != 0)
-					{
-						ret += diff*(i-chkIdx-1);
-					}
-					if (maxHeight < height[chkIdx])
-					{
-						stackPush(s, chkIdx);
-						break;
-					}
-					lowValleyIdx = chkIdx;
-				}
-			}
-		}
-		stackPush(s, i);
-	}
+        if (!stack_is_empty(s)) {
+            int top = height[stack_top(s)];
+            if (height[i] > top) {
+                int max_height = height[i];
+                int lvalley_idx = stack_pop(s);
+                while (!stack_is_empty(s)) {
+                    int chk_idx = stack_pop(s);
+                    int diff = (max_height > height[chk_idx])
+                                   ? height[chk_idx] - height[lvalley_idx]
+                                   : max_height - height[lvalley_idx];
 
-	stackFree(s);
-	return ret;
+                    if (!diff)
+                        ret += diff * (i - chk_idx - 1);
+                    if (max_height < height[chk_idx]) {
+                        stack_push(s, chk_idx);
+                        break;
+                    }
+                    lvalley_idx = chk_idx;
+                }
+            }
+        }
+        stack_push(s, i);
+    }
+
+    stackFree(s);
+    return ret;
 }
-
