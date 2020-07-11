1a611c0c4bf37c396d802175f95f6a8dc22fe034
ken.jin <kenpc.jin@moxa.com>
2020-07-12 00:35:44 +0800

    Rewrite #1046

--- LeetCode/1046_last-stone-weight/1046_last-stone-weight.c
+++ LeetCode/1046_last-stone-weight/1046_last-stone-weight.c
@@ -1,65 +1,53 @@
-void swap(int *a, int *b)
+static inline void swap(int *a, int *b)
 {
-	int tmp = *a;
-	*a = *b;
-	*b = tmp;
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
 }
 
-void heapify(int *arr, int size, int idx)
+static void heapify(int *arr, int size, int idx)
 {
-	int leftIdx = idx*2+1;
-	int rightIdx = idx*2+2;
-	int maxIdx = idx;
+    int left = (idx << 1) + 1;
+    int right = (idx << 1) + 2;
+    int max_idx = idx;
 
-	if (rightIdx < size && arr[rightIdx] > arr[maxIdx])
-	{
-		maxIdx = rightIdx;
-	}    
-	if (leftIdx < size && arr[leftIdx] > arr[maxIdx])
-	{
-		maxIdx = leftIdx;
-	}
+    if (right < size && arr[right] > arr[max_idx])
+        max_idx = right;
 
-	if (maxIdx != idx)
-	{
-		swap(&(arr[maxIdx]), &(arr[idx]));
-		heapify(arr, size, maxIdx);
-	}
+    if (left < size && arr[left] > arr[max_idx])
+        max_idx = left;
+
+    if (max_idx != idx) {
+        swap(&(arr[max_idx]), &(arr[idx]));
+        heapify(arr, size, max_idx);
+    }
 }
 
-void createHeap(int *arr, int size)
+static inline void create_heap(int *arr, int size)
 {
-	for (int i = (size-1)/2; i >= 0; i--)
-	{
-		heapify(arr, size, i);
-	}
+    for (int i = ((size - 1) >> 1); i >= 0; i--)
+        heapify(arr, size, i);
 }
 
-int lastStoneWeight(int* stones, int stonesSize)
+int lastStoneWeight(int *stones, int stonesSize)
 {
-	createHeap(stones, stonesSize);
-	while (stonesSize >= 4)
-	{
-		int max2 = (stonesSize > 2 && stones[2] > stones[1]) ? 2 : 1;            
-		int diff = abs(stones[0] - stones[max2]);
+    create_heap(stones, stonesSize);
+    while (stonesSize >= 4) {
+        int max2 = (stonesSize > 2 && stones[2] > stones[1]) ? 2 : 1;
+        int diff = abs(stones[0] - stones[max2]);
 
-		stones[0] = stones[stonesSize-1];
-		stones[max2] = (diff == 0 ? stones[stonesSize-2] : diff);
-		stonesSize = (diff == 0 ? stonesSize-2 : stonesSize-1);
+        stones[0] = stones[stonesSize - 1];
+        stones[max2] = (diff == 0 ? stones[stonesSize - 2] : diff);
+        stonesSize = (diff == 0 ? stonesSize - 2 : stonesSize - 1);
 
-		heapify(stones, stonesSize, max2);
-		heapify(stones, stonesSize, 0);
-	}
+        heapify(stones, stonesSize, max2);
+        heapify(stones, stonesSize, 0);
+    }
 
-	if (stonesSize == 3)
-	{
-		return abs(stones[0] - stones[1] - stones[2]);        
-	} else if (stonesSize == 2)
-	{
-		return abs(stones[0] - stones[1]);
-	} else
-	{
-		return (stonesSize == 1 ? stones[0] : 0);
-	}
+    if (stonesSize == 3)
+        return abs(stones[0] - stones[1] - stones[2]);
+    else if (stonesSize == 2)
+        return abs(stones[0] - stones[1]);
+    else
+        return (stonesSize == 1 ? stones[0] : 0);
 }
-
