7a904023a60d77cdb03c4df0e912748cdc502ee4
ken.jin <kenpc.jin@moxa.com>
2020-07-13 11:37:54 +0800

    Rewrite #201

--- LeetCode/0201_bitwise-and-of-numbers-range/0201_bitwise-and-of-numbers-range.c
+++ LeetCode/0201_bitwise-and-of-numbers-range/0201_bitwise-and-of-numbers-range.c
@@ -1,14 +1,10 @@
 
 int rangeBitwiseAnd(int m, int n)
 {
-	int base = 0;
-	int ret = INT_MAX;
-	while ((m & ret) != (n & ret)) 
-	{
-		int cur = (0x1 << base);
-		ret = (ret & ~(cur));
-		base++;
-	}
-	return m & ret;
+    int base = 0, ret = INT_MAX;
+    while ((m & ret) != (n & ret)) {
+        int cur = (0x1 << (base++));
+        ret &= ~cur;
+    }
+    return m & ret;
 }
-
