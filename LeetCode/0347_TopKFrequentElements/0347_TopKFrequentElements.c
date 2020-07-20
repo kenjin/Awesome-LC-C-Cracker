f95e1c05673de77390947c8e4b709a38c46e4c7e
ken.jin <kenpc.jin@moxa.com>
2020-07-20 20:54:54 +0800

    Rewrite #347

--- LeetCode/0347_TopKFrequentElements/0347_TopKFrequentElements.c
+++ LeetCode/0347_TopKFrequentElements/0347_TopKFrequentElements.c
@@ -15,71 +15,95 @@ Output: [1]
 Note:
 
 You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
-Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+Your algorithm's time complexity must be better than O(n log n), where n is the
+array's size.
 
  */
 
+typedef struct {
+    int used;
+    int val;
+    int dup;
+} DATA;
 
-struct nums_freq
-{
-    int num;
-    int ctr;
-};
+typedef struct __hash_table {
+    int size;
+    DATA *arr;
+} HASH;
 
-int compare(const void *p1, const void *p2)
+static inline int hash(HASH *obj, int key)
 {
-    return *(int *)p1 - *(int *)p2;
+    int r = key % obj->size;
+    return r < 0 ? r + obj->size : r;
 }
 
-int compareStruct(const void *p1, const void *p2)
+static inline HASH *hash_create(int size)
 {
-    const struct nums_freq *sp1 = (const struct nums_freq *) p1;
-    const struct nums_freq *sp2 = (const struct nums_freq *) p2;
-    return (int)(sp1->ctr - sp2->ctr);
+    HASH *obj = malloc(sizeof(HASH));
+    obj->size = size;
+    obj->arr = calloc(size, sizeof(DATA));
+    return obj;
 }
+
+static inline void hash_add(HASH *ht, int val)
+{
+    int index = hash(ht, val);
+    while (ht->arr[index].used) {
+        index++;
+        index %= ht->size;
+    }
+    ht->arr[index].used = 1;
+    ht->arr[index].val = val;
+    ht->arr[index].dup = 1;
+}
+
+static inline bool hash_update(HASH *ht, int target)
+{
+    int index = hash(ht, target);
+    while (ht->arr[index].used) {
+        if (ht->arr[index].val == target) {
+            ht->arr[index].dup += 1;
+            return true;
+        }
+
+        index++;
+        index %= ht->size;
+    }
+    return false;
+}
+
+static inline void hash_release(HASH *ht)
+{
+    free(ht->arr);
+    free(ht);
+}
+
+static int compare(const void *a, const void *b)
+{
+    return (*(DATA *) b).dup - (*(DATA *) a).dup;
+}
+
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int* topKFrequent(int* nums, int numsSize, int k, int* returnSize){
+int *topKFrequent(int *nums, int nums_sz, int k, int *ret_sz)
+{
+    HASH *h = hash_create(nums_sz + 1);
 
-    if (numsSize == 0 || k == 0)
-    {
-        *returnSize = 0;
-        return NULL;
-    }
-
-    qsort(nums, numsSize, sizeof(int), compare);
-    /* init the first element */
-    int currentIdx = 0;
-    struct nums_freq freq[numsSize];
-    memset(&freq, 0, sizeof(struct nums_freq)*numsSize);
-    freq[currentIdx].num = nums[0];
-    freq[currentIdx].ctr = 1;
-
-    for (int i = 1; i < numsSize; i++)
-    {
-        if ((nums[i] != nums[i-1]))
-        {
-            currentIdx++;
-            freq[currentIdx].num = nums[i];
-            freq[currentIdx].ctr = 1;
-        } else
-        {
-            freq[currentIdx].ctr++;
+    for (int i = 0; i < nums_sz; i++) {
+        if (!hash_update(h, nums[i])) {
+            hash_add(h, nums[i]);
         }
     }
 
-    int totalFreqIndex = currentIdx+1;
-    qsort(freq, totalFreqIndex, sizeof(struct nums_freq), compareStruct);
+    qsort(h->arr, nums_sz + 1, sizeof(DATA), compare);
 
-    int *ret = malloc(sizeof(int)*k);
-    for (int i = 0; i < k; i++)
-    {
-        int index = (totalFreqIndex-1)-i;
-        ret[i] = freq[index].num;
+    *ret_sz = k;
+    int *ret = malloc(sizeof(int) * k);
+    for (int i = 0; i < k; i++) {
+        ret[i] = h->arr[i].val;
     }
-    *returnSize = k;
 
+    hash_release(h);
     return ret;
 }
-
