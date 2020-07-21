ef4afb2c9e8a3f63fc0c630c1cceb80dcf7ee27a
ken.jin <kenpc.jin@moxa.com>
2020-07-21 17:21:35 +0800

    Rewrite #79

--- LeetCode/0079_WordSearch/0079_WordSearch.c
+++ LeetCode/0079_WordSearch/0079_WordSearch.c
@@ -2,66 +2,69 @@
 #79. Word Search
 
 Given a 2D board and a word, find if the word exists in the grid.
-The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
+The word can be constructed from letters of sequentially adjacent cell, where
+"adjacent" cells are those horizontally or vertically neighboring. The same
+letter cell may not be used more than once.
 
 Example:
-	board =
-	[
-  		['A','B','C','E'],
-    	['S','F','C','S'],
-	  	['A','D','E','E']
-	]
+    board =
+    [
+        ['A','B','C','E'],
+        ['S','F','C','S'],
+        ['A','D','E','E']
+    ]
 
-	Given word = "ABCCED", return true.
-	Given word = "SEE", return true.
-	Given word = "ABCB", return false.
+    Given word = "ABCCED", return true.
+    Given word = "SEE", return true.
+    Given word = "ABCB", return false.
 */
-bool search(char** board, char *word, int word_idx, int cur_row, int cur_col, int max_row, int max_col, int word_len)
-{
-	bool ret = false;
-	if (cur_row == max_row || cur_col == max_col || cur_row < 0 || cur_col < 0
-			|| word[word_idx] != board[cur_row][cur_col])
-	{
-		return false;
-	}
 
-	if ((word_len-1) == word_idx)
-	{
-		return true;
-	} else
-	{
-		char cur = board[cur_row][cur_col];
-		board[cur_row][cur_col] = -1;
-		if (search(board, word, word_idx+1, cur_row+1, cur_col, max_row, max_col, word_len))
-			return true;
-		if (search(board, word, word_idx+1, cur_row-1, cur_col, max_row, max_col, word_len))
-			return true;
-		if (search(board, word, word_idx+1, cur_row, cur_col+1, max_row, max_col, word_len))
-			return true;
-		if (search(board, word, word_idx+1, cur_row, cur_col-1, max_row, max_col, word_len))
-			return true;
-		board[cur_row][cur_col] = cur;
-	}    
-	return ret;
+static bool dfs(char **board,
+                char *word,
+                int widx,
+                int cur_row,
+                int cur_col,
+                int row_sz,
+                int col_sz,
+                int wlen)
+{
+    if (cur_row == row_sz || cur_col == col_sz || cur_row < 0 || cur_col < 0 ||
+        word[widx] != board[cur_row][cur_col])
+        return false;
+    /* End of the search */
+    if ((wlen - 1) == widx)
+        return true;
+
+    char cur = board[cur_row][cur_col];
+    board[cur_row][cur_col] = -1;
+    if (dfs(board, word, widx + 1, cur_row + 1, cur_col, row_sz, col_sz, wlen))
+        return true;
+    if (dfs(board, word, widx + 1, cur_row - 1, cur_col, row_sz, col_sz, wlen))
+        return true;
+    if (dfs(board, word, widx + 1, cur_row, cur_col + 1, row_sz, col_sz, wlen))
+        return true;
+    if (dfs(board, word, widx + 1, cur_row, cur_col - 1, row_sz, col_sz, wlen))
+        return true;
+    board[cur_row][cur_col] = cur;
+
+    return false;
 }
 
-bool exist(char** board, int boardRowSize, int *boardColSize, char* word) {
+bool exist(char **board, int board_sz, int *board_colsz, char *word)
+{
+    int wlen = strlen(word);
+    if (!wlen)
+        return true;
+    if (!board)
+        return false;
 
-	int len = strlen(word);
+    int col_sz = board_colsz[0];
+    for (int i = 0; i < board_sz; i++) {
+        for (int j = 0; j < col_sz; j++) {
+            if (dfs(board, word, 0, i, j, board_sz, col_sz, wlen))
+                return true;
+        }
+    }
 
-	if (len == 0)
-		return true;
-	if (board == NULL)
-		return false;
-
-	for (int i = 0; i < boardRowSize; i++)
-	{
-		for (int j = 0; j < boardColSize[0]; j++)
-		{
-			if (search(board, word, 0, i, j, boardRowSize, boardColSize[0], len) == true)
-				return true;
-		}
-	}
-
-	return false;
+    return false;
 }
