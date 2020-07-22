e07495aea6c3dd7688f0ca1cf093a5982063ddcc
ken.jin <kenpc.jin@moxa.com>
2020-07-22 18:15:18 +0800

    Rewrite #12

--- LeetCode/0012_integer-to-roman/0012_integer-to-roman.c
+++ LeetCode/0012_integer-to-roman/0012_integer-to-roman.c
@@ -1,43 +1,42 @@
-typedef struct dataInfo
+typedef struct {
+    int val;
+    char sym;
+} TRANSLATE;
+
+#define MAX_SYMBOL 7
+
+char *intToRoman(int num)
 {
-	int val;
-	char sym;
-}DATA;
+    TRANSLATE d[MAX_SYMBOL] = {{1, 'I'},   {5, 'V'},   {10, 'X'},  {50, 'L'},
+                               {100, 'C'}, {500, 'D'}, {1000, 'M'}};
 
-char * intToRoman(int num)
-{
-	DATA d[7] = {{1, 'I'}, {5, 'V'}, {10, 'X'}, {50, 'L'}, {100, 'C'}, {500, 'D'}, {1000, 'M'}};
+    char *ret = calloc(30, sizeof(char));
+    int ctr = 0;
+    for (int x = MAX_SYMBOL - 1; x >= 0; x--) {
+        int tmp = num / d[x].val;
+        num %= d[x].val;
+        /*  9 case */
+        if (x > 0 && tmp == 1 && (num / d[x - 1].val == 4)) {
+            ret[ctr] = d[x - 1].sym;
+            ret[ctr + 1] = d[x + 1].sym;
+            ctr += 2;
+            num %= d[x - 1].val;
+            continue;
+        }
+        /* 4 case */
+        if (tmp == 4) {
+            ret[ctr] = d[x].sym;
+            ret[ctr + 1] = d[x + 1].sym;
+            ctr += 2;
+            num %= d[x].val;
+            continue;
+        }
 
-	char *ret = calloc(30, sizeof(char));
-	int ctr = 0;
-	for (int x = 6; x >= 0; x--)
-	{
-		int tmp = num/d[x].val;
-		num %= d[x].val;
-
-		if (x > 0 && tmp == 1 && (num/d[x-1].val == 4)) // 9 case
-		{
-			ret[ctr] = d[x-1].sym;
-			ret[ctr+1] = d[x+1].sym;
-			ctr += 2;
-			num %= d[x-1].val;
-			continue;
-		}
-		if (tmp == 4) // 4 case
-		{
-			ret[ctr] = d[x].sym;
-			ret[ctr+1] = d[x+1].sym;
-			ctr += 2;
-			num %= d[x].val;
-			continue;
-		}
-		while (tmp > 0)
-		{
-			ret[ctr] = d[x].sym;
-			ctr++;
-			tmp--;
-		}
-	}
-	return ret;
+        while (tmp > 0) {
+            ret[ctr] = d[x].sym;
+            ctr++;
+            tmp--;
+        }
+    }
+    return ret;
 }
-
