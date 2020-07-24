dc2d0b2b4e20f92b1726cad4a4037f0528779ae7
ken.jin <kenpc.jin@moxa.com>
2020-07-24 15:43:41 +0800

    Rewrite #977

--- LeetCode/0977_Squares-of-a-Sorted-Array/0977_Squares-of-a-Sorted-Array.c
+++ LeetCode/0977_Squares-of-a-Sorted-Array/0977_Squares-of-a-Sorted-Array.c
@@ -2,54 +2,30 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int* sortedSquares(int* A, int ASize, int* returnSize)
+int *sortedSquares(int *A, int ASize, int *returnSize)
 {
-	*returnSize = ASize;
-	int *ret = malloc(sizeof(int)*ASize);
-	int *tmp = malloc(sizeof(int)*ASize);
+    *returnSize = ASize;
+    int *ret = malloc(sizeof(int) * ASize);
+    int neg_idx = -1;
+    for (int i = 0; i < ASize; i++) {
+        if (A[i] < 0)
+            neg_idx++;
+        A[i] = A[i] * A[i];
+    }
 
-	int i = 0;
-	while (i < ASize)
-	{
-		if (A[i] >= 0)
-		{
-			break;
-		}
-		tmp[i] = A[i];
-		i++;
-	}
+    int pos_idx = neg_idx + 1, ctr = 0;
+    while (neg_idx >= 0 && pos_idx < ASize) {
+        if (A[neg_idx] > A[pos_idx])
+            ret[ctr++] = A[pos_idx++];
+        else
+            ret[ctr++] = A[neg_idx--];
+    }
 
-	int ctr = 0;
-	int posIdx = i;
-	int negIdx = i-1;
-	while (posIdx < ASize && negIdx >= 0)
-	{
-		int p = A[posIdx]*A[posIdx];
-		int n = tmp[negIdx]*tmp[negIdx];
-		if (n < p)
-		{
-			ret[ctr] = n;
-			negIdx--;
-		} else
-		{
-			ret[ctr] = p;
-			posIdx++;
-		}
-		ctr++;   
-	}
-	while (posIdx < ASize)
-	{
-		ret[ctr] = A[posIdx]*A[posIdx];
-		posIdx++;
-		ctr++;  
-	}
-	while (negIdx >= 0)
-	{
-		ret[ctr] = tmp[negIdx]*tmp[negIdx];
-		negIdx--;
-		ctr++;  
-	}
-	free(tmp);
-	return ret;
+    while (neg_idx >= 0)
+        ret[ctr++] = A[neg_idx--];
+
+    while (pos_idx < ASize)
+        ret[ctr++] = A[pos_idx++];
+
+    return ret;
 }
-
