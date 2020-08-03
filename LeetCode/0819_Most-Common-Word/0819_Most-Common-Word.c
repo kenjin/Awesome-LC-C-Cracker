040c8312628878f4870c08d45821893f4562b34c
ken.jin <kenpc.jin@moxa.com>
2020-08-03 18:59:26 +0800

    Rewrite #819

--- LeetCode/0819_Most-Common-Word/0819_Most-Common-Word.c
+++ LeetCode/0819_Most-Common-Word/0819_Most-Common-Word.c
@@ -1,141 +1,106 @@
-#define TYPE_EMPTY          (-1)
-#define TYPE_BAN            (-2)
+#define TYPE_EMPTY (-1)
+#define TYPE_BAN (-2)
+#define MAX_STR_LEN (101)
 
-typedef struct hashT HASH;
-typedef struct dataInfo DATA;
+typedef struct __datainfo {
+    int ctr;
+    char str[MAX_STR_LEN]
+} DATA;
 
-struct dataInfo
+typedef struct __hashinfo {
+    int idx;
+    char str[MAX_STR_LEN];
+} HASH_NODE;
+
+typedef struct __hash {
+    HASH_NODE *arr;
+    int size;
+} HASH;
+
+static int hash(HASH *obj, char *str)
 {
-	int  ctr;
-	char str[101]
-};
+    size_t hash = 5381;
+    while (*str)
+        hash = 33 * hash ^ (unsigned char) *str++;
 
-struct hashInfo
-{
-	int idx;
-	char str[101];       
-};
-
-struct hashT
-{
-	int size;
-	int mod;
-	struct hashInfo **arr;
-};
-
-int hash(HASH *obj, char *str, int keyLen)
-{
-	size_t hash = 5381;
-	while (*str)
-	{
-		hash = 33 * hash ^ (unsigned char) *str++;
-	}
-	return hash % obj->size;
+    return hash % obj->size;
 }
 
-HASH* createHash(int size)
+static inline HASH *hash_create(int size)
 {
-	HASH *obj = malloc(sizeof(HASH));
-	obj->size = size;
-	obj->mod = size;
-	obj->arr = malloc(sizeof(struct hashInfo)*(size));
-	for (int x = 0; x < size; x++)
-	{
-		struct hashInfo *newInfo = calloc(1, sizeof(struct hashInfo));
-		newInfo->idx = TYPE_EMPTY;
-		obj->arr[x] = newInfo;
-	}
-	return obj;
+    HASH *obj = malloc(sizeof(HASH));
+    obj->size = size;
+    obj->arr = calloc(size, sizeof(HASH_NODE));
+    for (int x = 0; x < size; x++)
+        obj->arr[x].idx = TYPE_EMPTY;
+
+    return obj;
 }
 
-void addHash(HASH *ht, char *key, int idx) 
+static void hash_add(HASH *ht, char *key, int idx)
 {
-	int keyLen = strlen(key);
-	int index = hash(ht, key, keyLen);
-	while (ht->arr[index]->idx != TYPE_EMPTY) 
-	{
-		index++;
-		index %= ht->size;
-	}
+    int key_len = strlen(key);
+    int index = hash(ht, key);
+    while (ht->arr[index].idx != TYPE_EMPTY)
+        index = (index + 1) % (ht->size);
 
-	ht->arr[index]->idx = idx;
-	strncpy(ht->arr[index]->str, key, keyLen);
+    ht->arr[index].idx = idx;
+    strncpy(ht->arr[index].str, key, key_len);
 }
 
-int findHash(HASH *ht, char *key) 
+static int hash_find(HASH *ht, char *key)
 {
-	int keyLen = strlen(key);
-	int index = hash(ht, key, keyLen);
-	while (ht->arr[index]->idx != TYPE_EMPTY) 
-	{
-		if (!strncasecmp(ht->arr[index]->str, key, keyLen))
-		{
-			return ht->arr[index]->idx;
-		}
-		printf("..\n");
-		index++;
-		index %= ht->size;
-	}
-	return TYPE_EMPTY;
+    int key_len = strlen(key);
+    int index = hash(ht, key);
+    while (ht->arr[index].idx != TYPE_EMPTY) {
+        if (!strncasecmp(ht->arr[index].str, key, key_len))
+            return ht->arr[index].idx;
+        index = (index + 1) % (ht->size);
+    }
+    return TYPE_EMPTY;
 }
 
-void releaseHash(HASH *ht)
+static inline void hash_release(HASH *ht)
 {
-	for (int i = 0; i < ht->size; i++)
-	{
-		free(ht->arr[i]);
-	}
-	free(ht->arr);
-	free(ht);
+    free(ht->arr);
+    free(ht);
 }
 
-char * mostCommonWord(char * paragraph, char ** banned, int bannedSize)
+char *mostCommonWord(char *paragraph, char **banned, int bannedSize)
 {
-	HASH *h = createHash(500);
-	DATA d[101];
-	int dataSize = 0;
+    HASH *h = hash_create(500);
+    DATA d[MAX_STR_LEN];
+    int data_sz = 0;
 
-	for (int x = 0; x < bannedSize; x++)
-	{
-		addHash(h, banned[x], TYPE_BAN);
-	}
-	// transfer the string to lowercase
-	for (int x = 0; x < strlen(paragraph); x++)
-	{
-		if (paragraph[x] >= 'A' && paragraph[x] <= 'Z')
-		{
-			paragraph[x] = paragraph[x] - 'A' + 'a';
-		}
-	}
+    for (int x = 0; x < bannedSize; x++)
+        hash_add(h, banned[x], TYPE_BAN);
 
-	char *delim = " ,.!?;:'";
-	char *pch;
-	pch = strtok(paragraph, delim);
-	int maxIdx = 0; // The answer is unique
-	while (pch != NULL)
-	{
-		int ret = findHash(h, pch);           
-		if (TYPE_EMPTY == ret)
-		{
-			d[dataSize].ctr = 1;
-			sprintf(d[dataSize].str, "%s", pch);            
-			addHash(h, pch, dataSize);
-			dataSize++;
-		} else if (ret >= 0)
-		{
-			d[ret].ctr += 1;
-			if (d[ret].ctr > d[maxIdx].ctr)
-			{
-				maxIdx = ret;
-			}
-		}
-		pch = strtok(NULL, delim);
-	}      
+    /* transfer the string to lowercase */
+    for (int x = 0; x < strlen(paragraph); x++) {
+        if (paragraph[x] >= 'A' && paragraph[x] <= 'Z')
+            paragraph[x] = paragraph[x] - 'A' + 'a';
+    }
 
-	releaseHash(h);
-	char *ret = calloc(strlen(d[maxIdx].str)+1, sizeof(char));
-	strcpy(ret, d[maxIdx].str);    
-	return ret;
+    char *delim = " ,.!?;:'", *pch = NULL;
+    pch = strtok(paragraph, delim);
+    int max_idx = 0;
+    while (pch != NULL) {
+        int ret = hash_find(h, pch);
+        if (TYPE_EMPTY == ret) {
+            d[data_sz].ctr = 1;
+            sprintf(d[data_sz].str, "%s", pch);
+            hash_add(h, pch, data_sz);
+            data_sz++;
+        } else if (TYPE_BAN != ret) {
+            d[ret].ctr += 1;
+            if (d[ret].ctr > d[max_idx].ctr)
+                max_idx = ret;
+        }
+        pch = strtok(NULL, delim);
+    }
+
+    hash_release(h);
+    char *ret = calloc(strlen(d[max_idx].str) + 1, sizeof(char));
+    strcpy(ret, d[max_idx].str);
+    return ret;
 }
-
-
