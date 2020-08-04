13e8a351f358b33de8284dd1cd4ab55c0fab1f29
ken.jin <kenpc.jin@moxa.com>
2020-08-04 19:03:25 +0800

    Rewrite #414

--- LeetCode/0414_third-maximum-number/0414_third-maximum-number.c
+++ LeetCode/0414_third-maximum-number/0414_third-maximum-number.c
@@ -1,95 +1,79 @@
-typedef struct
-{
-	int *arr;
-	int size;
+typedef struct {
+    int *arr;
+    int size;
 } HEAP;
 
-void swap(int *a, int *b)
+static void swap(int *a, int *b)
 {
-	int tmp = *a;
-	*a = *b;
-	*b = tmp;
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
 }
 
-bool isEmptyHeap(HEAP *obj)
+static inline bool heap_is_empty(HEAP *obj)
 {
-	return (obj->size == 0 ? true : false);
+    return (obj->size == 0);
 }
 
 void heapify(HEAP *obj, int cur_idx)
 {
-	int max_idx = cur_idx;
-	int l_idx = cur_idx*2 + 1;
-	int r_idx = cur_idx*2 + 2;
+    int max_idx = cur_idx;
+    int l_idx = (cur_idx << 1) + 1;
+    int r_idx = (cur_idx << 1) + 2;
 
-	if (l_idx < obj->size && obj->arr[max_idx] < obj->arr[l_idx])
-	{
-		max_idx = l_idx;
-	}
-	if (r_idx < obj->size && obj->arr[max_idx] < obj->arr[r_idx])
-	{
-		max_idx = r_idx;
-	}
-
-	// The max is left child or right child
-	if (max_idx != cur_idx)
-	{
-		swap(&(obj->arr[max_idx]), &(obj->arr[cur_idx]));
-		heapify(obj, max_idx);
-	}
+    if (l_idx < obj->size && obj->arr[max_idx] < obj->arr[l_idx])
+        max_idx = l_idx;
+    if (r_idx < obj->size && obj->arr[max_idx] < obj->arr[r_idx])
+        max_idx = r_idx;
+    /* The max is left child or right child */
+    if (max_idx != cur_idx) {
+        swap(&(obj->arr[max_idx]), &(obj->arr[cur_idx]));
+        heapify(obj, max_idx);
+    }
 }
 
-int delMaxHeap(HEAP *obj)
+int heap_del(HEAP *obj)
 {
-	int ret = obj->arr[0];
-	obj->arr[0] = obj->arr[obj->size-1];    
-	obj->size -= 1;
-	heapify(obj, 0);    
-	return ret;
+    int ret = obj->arr[0];
+    obj->arr[0] = obj->arr[obj->size - 1];
+    obj->size -= 1;
+    heapify(obj, 0);
+
+    return ret;
 }
 
-HEAP* createHeap(int *n, int nSize)
+static inline HEAP *heap_create(int *n, int size)
 {
-	HEAP *obj = malloc(sizeof(HEAP));
-	obj->arr = malloc(sizeof(int)*nSize);
-	memcpy(obj->arr, n, sizeof(int)*nSize);
-	obj->size = nSize;
+    HEAP *obj = malloc(sizeof(HEAP));
+    obj->arr = n;
+    obj->size = size;
 
-	for (int i = (nSize-1)/2; i >= 0; i--)
-	{
-		heapify(obj, i);
-	}
-
-	return obj;
+    for (int i = (size - 1) / 2; i >= 0; i--)
+        heapify(obj, i);
+    return obj;
 }
 
-void destroyHeap(HEAP *obj)
+static inline void heap_release(HEAP *obj)
 {
-	free(obj->arr);
-	free(obj);
+    free(obj);
 }
 
-int thirdMax(int* nums, int numsSize)
+int thirdMax(int *nums, int numsSize)
 {
-	HEAP *h = createHeap(nums, numsSize);
+    HEAP *h = heap_create(nums, numsSize);
+    int ret, pre, tmp, del_num = 1;
+    ret = heap_del(h);
+    pre = ret;
+    while (!heap_is_empty(h)) {
+        tmp = heap_del(h);
+        del_num = (tmp == pre ? del_num : del_num + 1);
+        pre = tmp;
+        if (del_num == 3) {
+            ret = tmp;
+            break;
+        }
+    }
 
-	int ret, pre, tmp, delNums = 1;
-	ret = delMaxHeap(h);
-	pre = ret;
-	while (!isEmptyHeap(h))
-	{        
-		tmp = delMaxHeap(h);        
-		delNums = (tmp == pre ? delNums : delNums+1);
-		pre = tmp;
-		if (delNums == 3)
-		{
-			ret = tmp;
-			break;
-		}
-	}
-
-	destroyHeap(h);
-	return ret;
+    heap_release(h);
+    return ret;
 }
-
-
