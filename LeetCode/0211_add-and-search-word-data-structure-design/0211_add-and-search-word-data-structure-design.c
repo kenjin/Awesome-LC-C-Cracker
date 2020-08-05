7ae3563eb36c8f10401462a53a9487cca36ed09b
ken.jin <kenpc.jin@moxa.com>
2020-08-05 17:54:35 +0800

    Rewrite #211

--- LeetCode/0211_add-and-search-word-data-structure-design/0211_add-and-search-word-data-structure-design.c
+++ LeetCode/0211_add-and-search-word-data-structure-design/0211_add-and-search-word-data-structure-design.c
@@ -1,99 +1,82 @@
 
-typedef struct trieInfo
-{
-	char add;
-	struct trieInfo *child[26];
+typedef struct __trieInfo {
+    char used;
+    struct __trieInfo *child[26];
 } TRIE;
 
-typedef struct 
-{
-	TRIE *t;
+typedef struct {
+    TRIE *t;
 } WordDictionary;
+
+static void word_dict_node_release(TRIE *t)
+{
+    if (!t)
+        return;
+
+    for (int i = 0; i < 26; i++)
+        word_dict_node_release(t->child[i]);
+    free(t);
+}
+
+static bool word_dict_search(TRIE *root, char *word)
+{
+    char *cur = word;
+    while (*cur) {
+        if (*cur == '.') {
+            cur++;
+            for (int x = 0; x < 26; x++) {
+                if (root->child[x] && word_dict_search(root->child[x], cur))
+                    return true;
+            }
+            return false;
+        }
+
+        int idx = *cur - 'a';
+        if (!root->child[idx])
+            return false;
+        root = root->child[idx];
+        cur++;
+    }
+
+    return root->used ? true : false;
+}
 
 /** Initialize your data structure here. */
 
-WordDictionary* wordDictionaryCreate() 
+WordDictionary *wordDictionaryCreate()
 {
-	WordDictionary *obj = malloc(sizeof(WordDictionary));
-	obj->t = calloc(1, sizeof(TRIE));
-
-	return obj;
+    WordDictionary *obj = malloc(sizeof(WordDictionary));
+    obj->t = calloc(1, sizeof(TRIE));
+    return obj;
 }
 
 /** Adds a word into the data structure. */
-void wordDictionaryAddWord(WordDictionary* obj, char * word) 
-{       
-	TRIE *root = obj->t;
-	char *tmpWord = word;
-	while (*tmpWord)
-	{
-		int idx = *tmpWord - 'a';
+void wordDictionaryAddWord(WordDictionary *obj, char *word)
+{
+    TRIE *root = obj->t;
+    char *cur = word;
+    while (*cur) {
+        int idx = *cur - 'a';
+        if (!root->child[idx])
+            root->child[idx] = calloc(1, sizeof(TRIE));
+        root = root->child[idx];
+        cur++;
+    }
 
-		if (root->child[idx] == NULL)
-		{
-			root->child[idx] = calloc(1, sizeof(TRIE));
-		}
-		root = root->child[idx];
-		tmpWord++;
-	}
-
-	root->add = 1;
+    root->used = 1;
 }
 
-bool wordDictionarySearchHeper(TRIE *root, char * word) 
+/** Returns if the word is in the data structure. A word could contain the dot
+ * character '.' to represent any one letter. */
+bool wordDictionarySearch(WordDictionary *obj, char *word)
 {
-	char *tmpWord = word;
-	while (*tmpWord)
-	{
-		if (*tmpWord == '.')
-		{
-			tmpWord++;
-			for (int x = 0; x < 26; x++)
-			{
-				if (root->child[x] != NULL && wordDictionarySearchHeper(root->child[x], tmpWord))
-				{
-					return true;
-				}                
-			}
-			return false;
-		}
-
-		int idx = *tmpWord - 'a';
-		if (root->child[idx] == NULL)
-		{
-			return false;
-		}
-		root = root->child[idx];
-		tmpWord++;
-	}
-
-	return root->add ? true : false;
+    return word_dict_search(obj->t, word);
 }
 
-/** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
-bool wordDictionarySearch(WordDictionary* obj, char * word) 
+void wordDictionaryFree(WordDictionary *obj)
 {
-	return wordDictionarySearchHeper(obj->t, word);
-}
-
-void wordDictionaryFreeHelper(TRIE *t)
-{
-	if (NULL == t)
-	{
-		return;
-	}
-	for (int i = 0; i < 26; i++)
-	{
-		wordDictionaryFreeHelper(t->child[i]);
-	}
-
-	free(t);
-}
-
-void wordDictionaryFree(WordDictionary* obj) 
-{
-	wordDictionaryFreeHelper(obj->t);
-	free(obj);
+    word_dict_node_release(obj->t);
+    free(obj);
 }
 
 /**
