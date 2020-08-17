9f63df6ad74193a4452dcc0b54d6e42bd9aa99a3
ken.jin <kenjcfar@gmail.com>
2020-08-17 13:49:16 +0800

    Impl stack via singly-linked list

--- ds/impl_stack.c
+++ ds/impl_stack.c
@@ -1,4 +1,58 @@
+#if 0
+typedef struct __list_data {
+    int val;
+    struct __list_data *next;
+} DATA;
 
+typedef struct {
+    DATA *head;
+} STACK;
+
+static inline STACK *stack_create()
+{
+    STACK *obj = malloc(sizeof(STACK));
+    obj->head = NULL;
+    return obj;
+}
+
+static void stack_push(STACK *obj, int val)
+{
+    DATA *new_node = calloc(1, sizeof(DATA));
+    new_node->val = val;
+    if (obj->head)
+        new_node->next = obj->head;
+
+    obj->head = new_node;
+}
+
+static inline bool stack_is_empty(STACK *obj)
+{
+    return (obj->head == NULL);
+}
+
+static void stack_pop(STACK *obj)
+{
+    DATA *del = obj->head;
+    obj->head = del->next;
+    free(del);
+}
+/* return value of head data node */
+static int stack_top(STACK *obj)
+{
+    return obj->head->val;
+}
+
+static inline void stack_release(STACK *obj)
+{
+    DATA *cur = obj->head;
+    while (cur) {
+        DATA *del = cur;
+        cur = cur->next;
+        free(del);
+    }
+    free(obj);
+}
+#endif
 
 typedef struct __list {
     int val;
