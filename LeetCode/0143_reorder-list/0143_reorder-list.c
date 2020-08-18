9384b138ba7369373f61f35078d9a2ef6adc8017
ken.jin <kenjcfar@gmail.com>
2020-08-18 15:07:19 +0800

    Rewrite #143

--- LeetCode/0143_reorder-list/0143_reorder-list.c
+++ LeetCode/0143_reorder-list/0143_reorder-list.c
@@ -1,43 +1,43 @@
-void reorderList(struct ListNode* head)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+typedef struct ListNode NODE;
+void reorderList(struct ListNode *head)
 {
-	if (head == NULL)
-	{
-		return;
-	}
+    if (!head)
+        return;
 
-	struct ListNode *fast = head;
-	struct ListNode *slow = head;    
-	int slowLen = 0;
-	while (fast && fast->next)
-	{
-		slowLen++;
-		fast = fast->next->next;
-		slow = (fast == NULL ? slow : slow->next);
-	}
+    NODE *p1 = head, *p2 = head;
+    int len = 0;
+    while (p2 && p2->next) {
+        len++;
+        p2 = p2->next->next;
+        p1 = (!p2 ? p1 : p1->next);
+    }
 
-	// simple stack
-	struct ListNode **s = malloc(sizeof(struct ListNode *)*(slowLen+1));
-	int ctr = 0;
-	struct ListNode *tmp = slow->next;
-	while (tmp)
-	{
-		s[ctr] = tmp;
-		ctr++;
-		tmp = tmp->next;
-	}
+    NODE **stack = malloc(sizeof(NODE *) * (len + 1));
+    int s_ctr = 0;
+    p1 = p1->next;
+    while (p1) {
+        stack[s_ctr++] = p1;
+        p1 = p1->next;
+    }
+    NODE *cur = head, *tmp = NULL;
+    for (int i = s_ctr - 1; i >= 0; i--) {
+        tmp = cur->next;
+        cur->next = stack[i];
+        stack[i]->next = tmp;
+        cur = tmp;
+    }
 
-	for (int i = ctr-1; i >=0 ;i--)
-	{        
-		tmp = head->next;
-		head->next = s[i];
-		s[i]->next = tmp;
-		head = tmp;
-	}
+    if (tmp)
+        tmp->next = NULL;
 
-	if (tmp)
-	{
-		tmp->next = NULL;
-	}
-
-	free(s);
+    free(stack);
 }
