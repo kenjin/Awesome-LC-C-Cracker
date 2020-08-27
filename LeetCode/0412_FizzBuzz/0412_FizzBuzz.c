a95ddd8d417a9cb55604e6deacc44fd46283a3fb
ken.jin <kenjcfar@gmail.com>
2020-08-27 18:10:22 +0800

    Rewrite #412

--- LeetCode/0412_FizzBuzz/0412_FizzBuzz.c
+++ LeetCode/0412_FizzBuzz/0412_FizzBuzz.c
@@ -1,43 +1,23 @@
-/**
 
-412. Fizz Buzz [E]
-Ref: https://leetcode.com/problems/fizz-buzz/
-
- */
 
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-
-void createStr(int ***ret, int *returnSize, char *str, int size)
+char **fizzBuzz(int n, int *return_sz)
 {
-	*returnSize += 1;
-	(*ret)[(*returnSize)-1] = calloc(size, sizeof(char));
-	strcpy((*ret)[(*returnSize)-1], str);    
-}
+    char **ret = malloc(sizeof(char *) * n);
+    *return_sz = n;
+    int def_sz = (n < 5 ? 5 : n + 1);
 
-char ** fizzBuzz(int n, int* returnSize)
-{
-	char **ret = malloc(sizeof(char*)*n);
-	*returnSize = 0;
-	for (int i = 1; i <= n; i++)
-	{
-		if (i % 15 == 0)
-		{
-			createStr(&ret, returnSize, "FizzBuzz", 9);
-		} else if (i % 3 == 0)
-		{
-			createStr(&ret, returnSize, "Fizz", 5);
-		} else if (i % 5 == 0)
-		{
-			createStr(&ret, returnSize, "Buzz", 5);
-		} else
-		{
-			char chNum[12] = {0};
-			sprintf(chNum, "%d", i);
-			createStr(&ret, returnSize, chNum, strlen(chNum)+1);
-		}
-	}
+    for (int i = 1; i <= n; i++) {
+        ret[i - 1] = calloc(def_sz, sizeof(char));
+        if (0 == i % 3)
+            strcat(ret[i - 1], "Fizz");
+        if (0 == i % 5)
+            strcat(ret[i - 1], "Buzz");
+        if (0 != i % 5 && 0 != i % 3)
+            sprintf(ret[i - 1], "%d", i);
+    }
 
-	return ret;
+    return ret;
 }
