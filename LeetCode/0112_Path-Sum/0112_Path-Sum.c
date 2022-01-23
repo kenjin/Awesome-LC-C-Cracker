24d45bf1772cc891e4815789753717c70684c098
Ken Jin <kenjcfar@gmail.com>
2022-01-24 02:58:23 +0800

    Rewrite #112

--- LeetCode/0112_Path-Sum/0112_Path-Sum.c
+++ LeetCode/0112_Path-Sum/0112_Path-Sum.c
@@ -15,36 +15,13 @@ Ref: https://leetcode.com/problems/path-sum/
  * };
  */
 
-bool calSum(struct TreeNode* root, int sum, int target)
-{    
-	if (root == NULL)
-	{
-		return (sum == target ? true : false);
-	}
 
-	target += root->val;
-	bool bL = false;
-	bool bR = false;
-
-	if (root->right == NULL || root->left != NULL)
-	{
-		bL = calSum(root->left, sum, target);
-	}
-
-	if ((bL == false) && /* Branch pruning */
-			(root->left == NULL || root->right != NULL))
-	{
-		bR = calSum(root->right, sum, target);
-	}
-
-	return (bL | bR);
-}
-
-bool hasPathSum(struct TreeNode* root, int sum)
+bool hasPathSum(struct TreeNode* root, int targetSum)
 {
-	if (root == NULL)
-	{
+	if (!root)
 		return false;
-	}
-	return calSum(root, sum, 0);
+	if (!root->left && !root->right)
+		return (targetSum == root->val);
+
+	return (hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum - root->val));
 }
