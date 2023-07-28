0cc8cad16f4a8a88f9fb57fcb6f6f522572588ee
Ken Jin <kenjcfar@gmail.com>
2023-07-28 14:32:02 +0800

    0078[M]: Rewrite dfs solution

--- leetcode-c-sol/0078_Subsets/0078_Subsets.c
+++ leetcode-c-sol/0078_Subsets/0078_Subsets.c
@@ -1,76 +1,27 @@
-/**
-  78. Subsets [M]
-Ref: https://leetcode.com/problems/subsets/
-
-Given a set of distinct integers, nums, return all possible subsets (the power
-set).
-
-Note: The solution set must not contain duplicate subsets.
-
-Example:
-Input: nums = [1,2,3]
-Output:
-[
-[3],
-[1],
-[2],
-[1,2,3],
-[1,3],
-[2,3],
-[1,2],
-[]
-]
-
- */
-#define MALLOC_UNIT 500
-
-static void dfs(int *nums,
-                int *tmp,
-                int tmp_sz,
-                int cur,
-                int nums_sz,
-                int ***ret,
-                int *ret_sz,
-                int **ret_col_sz)
+static void dfs_handler(int* nums, int cur, int num_sz, int *tmp, int tmp_ctr, int **ret, int *ret_sz, int *ret_col_sz)
 {
-    if (cur == nums_sz) {
-        if (*ret_sz % MALLOC_UNIT == 0) {
-            *ret = realloc(*ret, sizeof(int *) * (*ret_sz + MALLOC_UNIT));
-            *ret_col_sz =
-                realloc(*ret_col_sz, sizeof(int) * (*ret_sz + MALLOC_UNIT));
-        }
-
-        if (!tmp_sz) {
-            (*ret)[*ret_sz] = NULL;
-            (*ret_col_sz)[*ret_sz] = 0;
-        } else {
-            (*ret)[*ret_sz] = malloc(sizeof(int) * tmp_sz);
-            (*ret_col_sz)[*ret_sz] = tmp_sz;
-            memcpy((*ret)[*ret_sz], tmp, sizeof(int) * tmp_sz);
-        }
-        *ret_sz += 1;
+    if (cur == num_sz) {
+        int *res = malloc(sizeof(int)*tmp_ctr);
+        memcpy(res, tmp, sizeof(int)*tmp_ctr);
+        ret[*ret_sz] = res;
+        ret_col_sz[*ret_sz] = tmp_ctr;
+        (*ret_sz)++;
         return;
     }
 
-    tmp[tmp_sz] = nums[cur];
-    dfs(nums, tmp, tmp_sz + 1, cur + 1, nums_sz, ret, ret_sz, ret_col_sz);
-    dfs(nums, tmp, tmp_sz, cur + 1, nums_sz, ret, ret_sz, ret_col_sz);
+    dfs_handler(nums, cur + 1, num_sz, tmp, tmp_ctr, ret, ret_sz, ret_col_sz);
+    tmp[tmp_ctr] = nums[cur];
+    dfs_handler(nums, cur + 1, num_sz, tmp, tmp_ctr + 1, ret, ret_sz, ret_col_sz);
 }
 
-/**
- * Return an array of arrays of size *returnSize.
- * The sizes of the arrays are returned as *returnColumnSizes array.
- * Note: Both returned array and *columnSizes array must be malloced, assume
- * caller calls free().
- */
-int **subsets(int *nums, int nums_sz, int *ret_sz, int **ret_col_sz)
+int** subsets(int* nums, int num_sz, int* ret_sz, int** ret_col_sz)
 {
-    int *tmp = malloc(sizeof(int) * nums_sz);
-    int **ret = malloc(sizeof(int *));
-    *ret_col_sz = malloc(sizeof(int));
+    int *tmp = malloc(sizeof(int)*num_sz);
+    int **ret = malloc(sizeof(int*)*(1 << num_sz));
+    *ret_col_sz = malloc(sizeof(int)*(1 << num_sz));
     *ret_sz = 0;
-    dfs(nums, tmp, 0, 0, nums_sz, &ret, ret_sz, ret_col_sz);
-    free(tmp);
+    dfs_handler(nums, 0, num_sz, tmp, 0, ret, ret_sz, *ret_col_sz);
 
+    free(tmp);
     return ret;
 }
