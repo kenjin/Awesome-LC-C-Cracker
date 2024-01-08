87e6d71b8ceb67b62c3826d57c7cee0f0691eebf
Ken Jin <kenjcfar@gmail.com>
2024-01-08 09:51:39 +0800

    0938[E]: Rewrite proper pruning solution

--- leetcode-c-sol/0938_range-sum-of-bst/0938_range-sum-of-bst.c
+++ leetcode-c-sol/0938_range-sum-of-bst/0938_range-sum-of-bst.c
@@ -7,24 +7,15 @@
  * };
  */
 
-
-int rangeSumBST(struct TreeNode* root, int L, int R)
+int rangeSumBST(struct TreeNode* root, int low, int high)
 {
-	if (root == NULL)
-	{
-		return 0;
-	}
+    if (!root)
+        return 0;
 
-	int rRet = 0, lRet = 0;
-	if (root->val > L)
-	{
-		lRet = rangeSumBST(root->left, L, R);
-	}
-	if (root->val < R)
-	{
-		rRet = rangeSumBST(root->right, L, R);
-	}
+    if (root->val < low)
+        return rangeSumBST(root->right, low, high);
+    if (root->val > high)
+        return rangeSumBST(root->left, low, high);
 
-	return  (L <= root->val && root->val <= R) ? rRet + lRet + root->val : rRet + lRet;
+    return root->val + rangeSumBST(root->right, low, high) + rangeSumBST(root->left, low, high);
 }
-
