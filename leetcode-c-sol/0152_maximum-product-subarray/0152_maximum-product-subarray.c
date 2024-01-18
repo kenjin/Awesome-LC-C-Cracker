c26ccda2e8d47b8228fd0b717c3b336e673cb491
Ken Jin <kenjcfar@gmail.com>
2024-01-18 21:52:07 +0800

    0152[M]: Rewrite wo redundant dp space

--- leetcode-c-sol/0152_maximum-product-subarray/0152_maximum-product-subarray.c
+++ leetcode-c-sol/0152_maximum-product-subarray/0152_maximum-product-subarray.c
@@ -1,32 +1,21 @@
 
+#define MAX(a, b) (a > b ? a : b)
+#define MIN(a, b) (a < b ? a : b)
+
 int maxProduct(int* nums, int numsSize)
 {
-	int max = nums[0];
-	int ret = 0;
-	int *dpPos = malloc(sizeof(int)*numsSize);
-	int *dpNeg = malloc(sizeof(int)*numsSize);
-
-	memcpy(dpPos, nums, sizeof(int)*numsSize);
-	memcpy(dpNeg, nums, sizeof(int)*numsSize);    
-	for (int i = 1; i < numsSize; i++)
-	{
-		int tmp1 = nums[i]*dpPos[i-1];
-		int tmp2 = nums[i]*dpNeg[i-1];        
-		if (tmp1 > tmp2)
-		{
-			dpPos[i] = nums[i] > tmp1 ? nums[i] : tmp1;
-			dpNeg[i] = nums[i] < tmp2 ? nums[i] : tmp2;
-		} else
-		{
-			dpPos[i] = nums[i] > tmp2 ? nums[i] : tmp2;
-			dpNeg[i] = nums[i] < tmp1 ? nums[i] : tmp1;
-		}
-
-		max = max > dpPos[i] ? max :dpPos[i];
-	}
-
-	free(dpPos);
-	free(dpNeg);
-	return max;
+    int dp_pos = nums[0], dp_neg = nums[0], ret = nums[0];
+    for (int i = 1; i < numsSize; i++) {
+        int tmp1 = nums[i] * dp_pos;
+        int tmp2 = nums[i] * dp_neg;
+        if (tmp1 > tmp2){
+            dp_pos = MAX(tmp1, nums[i]);
+            dp_neg = MIN(tmp2, nums[i]);
+        } else {
+            dp_pos = MAX(tmp2, nums[i]);
+            dp_neg = MIN(tmp1, nums[i]);
+        }
+        ret = MAX(ret, dp_pos);
+    }
+    return ret;
 }
-
