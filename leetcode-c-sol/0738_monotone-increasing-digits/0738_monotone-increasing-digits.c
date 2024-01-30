5f42f3463121cc6c5284185b1e092232305aa253
Ken Jin <kenjcfar@gmail.com>
2024-01-30 15:47:45 +0800

    0738[M]: Add an easily readable solution

--- leetcode-c-sol/0738_monotone-increasing-digits/0738_monotone-increasing-digits.c
+++ leetcode-c-sol/0738_monotone-increasing-digits/0738_monotone-increasing-digits.c
@@ -1,31 +1,39 @@
-
-int monotoneIncreasingDigits(int N)
+static inline char* reverse(char *str)
 {
-	int tmp = N, nineSum = 9, base = 10;
-	int pre = tmp % 10;
-	int sum = pre;
-	tmp /= 10;
-	while (tmp)
-	{
-		int cur = tmp % 10;        
-		sum += base*cur;
-
-		if (cur > pre)
-		{
-			int add = base*(cur-1);
-			N = N - sum + base*(cur-1) + nineSum;
-			sum = base*(cur-1) + nineSum;
-			pre = cur-1;
-		} else
-		{
-			pre = cur;
-		}
-
-		tmp /= 10;
-		base *= 10;
-		nineSum = nineSum*10+9;
-	}
-
-	return N;
+    int tail = strlen(str) - 1, head = 0;
+    while (head < tail) {
+        char t = str[head];
+        str[head] = str[tail];
+        str[tail] = t;
+        head++;
+        tail--;
+    }
+    return str;
 }
 
+int monotoneIncreasingDigits(int n)
+{
+    char *str = calloc(11, sizeof(char));
+    int cur_digit = 0, all9_digit = 0;
+    int pre = n % 10, cur = n / 10;
+    while (cur) {
+        int tmp = cur % 10;
+        if (tmp <= pre) {
+            str[cur_digit] = pre + '0';
+            pre = tmp;
+        } else {
+            for (int i = cur_digit; i >= all9_digit; i--)
+                str[i] = '9';
+            // Avoid duplicate assignment
+            all9_digit = cur_digit;   
+            pre = tmp - 1;
+        }
+        cur_digit++;
+        cur /= 10;
+    }
+
+    if (pre)
+        str[cur_digit] = pre + '0';
+
+    return atoi(reverse(str));
+}
