33aca580fefef75c71dced5a639986f9472690eb
Ken Jin <kenjcfar@gmail.com>
2024-03-24 10:13:27 +0800

    0142[M] Rewrite the solution

--- leetcode-c-sol/0142_Linked-List-CycleII/0142_Linked-List-CycleII.c
+++ leetcode-c-sol/0142_Linked-List-CycleII/0142_Linked-List-CycleII.c
@@ -1,87 +1,46 @@
-/**
-
-142. Linked List Cycle II [Medium]
-
-Given a linked list, return the node where the cycle begins. If there 
-is no cycle, return null.
-
-To represent a cycle in the given linked list, we use an integer pos 
-which represents the position (0-indexed) in the linked list where tail 
-connects to. If pos is -1, then there is no cycle in the linked list.
-
-Note: Do not modify the linked list.
-
- 
-
-Example 1:
-
-Input: head = [3,2,0,-4], pos = 1
-Output: tail connects to node index 1
-
-Explanation: There is a cycle in the linked list, where tail connects to the second node.
-
-
+/*
+ * Approach: Fast-Slow pointers
+ *
+ * [R]: root
+ * [M]: meet point
+ * [*]: start point in cycle
+ *
+ *       "X"      "Y"
+ * [R] ------[*]------- [M]---
+ *            |     "Z"      |
+ *             ---------------
+ *
+ * Distance(Slow) = X + Y          (R->*->M)
+ * Distance(Fast) = X + Y + Y + Z  (R->*->M->*->M)
+ * => Fast travels with double speed and two pointers meet at the same time.
+ * => 2*Distance(Slow) = Distance(Fast)
+ * => 2*(X+Y) = X+Y+Y+Z
+ * =>       X = Z
+ *
+ * It means that [R] and [M] will meet at [*] after X moves of [R] and Z moves of [M].
  */
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     struct ListNode *next;
- * };
- */
-
-struct ListNode *chechCycle(struct ListNode *head) 
+static inline struct ListNode* find_meet(struct ListNode* head)
 {
-	struct ListNode *fast = head;
-	struct ListNode *slow = head;
-
-	while (slow != NULL && fast != NULL && fast->next != NULL)
-	{
-		slow = slow->next;
-		fast = fast->next->next;
-		if (slow == fast)
-		{
-			return slow;
-		}
-	}
-	return NULL;
+    struct ListNode *fast = head, *slow = head;
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
+            return slow;
+    }
+    return NULL;
 }
 
-struct ListNode *detectCycle(struct ListNode *head) 
+struct ListNode* detectCycle(struct ListNode* head)
 {
-	struct ListNode *meetPoint = chechCycle(head);
+    struct ListNode *meet = find_meet(head);    
+    while (meet) {
+        if (head == meet)
+            return head;
+        head = head->next;
+        meet = meet->next;
+    }   
 
-	if (meetPoint == NULL)
-	{
-		return NULL;
-	} else
-	{
-		/* [R]: root. 
-		 * [M]: meet point
-		 * [*]: start point in  cycle
-		 *
-		 *       "X"      "Y"
-		 * [R] ------[*]------- [M]---
-		 *            |     "Z"      |
-		 *		       ---------------
-		 *
-		 * Distance(Slow) = X + Y          (R->*->M)
-		 * Distance(Fast) = X + Y + Y + Z  (R->*->M->*->M)
-		 * => Fast travels with double speed and two pointer meet at same time.
-		 * => 2*Distance(Slow) = Distance(Fast)
-		 * => 2(X+Y) = X+Y+Y+Z
-		 * =>      X = Z
-		 *
-		 * It means that [M] and [R] after a X/Z moves will meet at [*]
-		 */
-
-		struct ListNode *root = head;   
-		while (meetPoint != NULL && root != NULL && meetPoint != root)
-		{
-			meetPoint = meetPoint->next;
-			root = root->next;
-		}
-		return root;
-	}    
+    return NULL;
 }
