ae3ed15d725daed4c77be98c7613c5d82b6b8f5c
Ken Jin <kenjcfar@gmail.com>
2024-04-11 15:12:00 +0800

    0402[M]: Refine stack solution

--- leetcode-c-sol/0402_remove-k-digits/0402_remove-k-digits.c
+++ leetcode-c-sol/0402_remove-k-digits/0402_remove-k-digits.c
@@ -1,50 +1,49 @@
+typedef struct _stack {
+    char* arr;
+    int ctr;
+    int size;
+} STACK;
 
-char * removeKdigits(char * num, int k)
-{
-	int len = strlen(num);
-	char *tmpNum = calloc(len+1, sizeof(char));
-	strcpy(tmpNum, num);
-	for (int i = 0; i < k; i++)
-	{
-		int pre = -1;
-		for (int x = 0; x < len; x++)
-		{
-			if (tmpNum[x] == 'x')
-			{
-				continue;
-			}
+#define stack_init(SZ) {.arr = calloc(SZ, sizeof(char)), .ctr = 0, .size = SZ}
+#define stack_dinit(S)      do {free(S.arr);} while(0)
+#define stack_push(S, IDX) do {S.arr[S.ctr++] = IDX;} while (0)
+#define stack_pop(S)       (S.arr[--S.ctr])
+#define stack_cur_size(S)  (s.ctr)
+#define stack_top(S)       (S.arr[S.ctr - 1])
+#define stack_is_empty(S)  (S.ctr == 0 ? true : false)
 
-			if (pre == -1)
-			{
-				pre = x;
-				continue;
-			}
+char* removeKdigits(char* num, int k) {
+    int len = strlen(num), idx = 0;
+    STACK s = stack_init(len);
+    // Monotonic stack with ascending elements
+    while (idx < len && k > 0) {
+        if (!stack_is_empty(s) && stack_top(s) > num[idx]) {
+            stack_pop(s);
+            k--;
+        } else {
+            stack_push(s, num[idx++]);
+        }
+    }
+    // Push the rest of digit
+    while (idx < len)
+        stack_push(s, num[idx++]);
+    // Pop the rest of k digit from monotonic stack
+    while (k > 0) {
+        stack_pop(s);
+        k--;
+    }
 
-			if (tmpNum[x] < tmpNum[pre])
-			{
-				break;
-			} else
-			{
-				pre = x;
-			}
-		}
-		tmpNum[pre] = 'x';
-	}
+    /* remove leading zero */
+    bool lead_zero = true;
+    int ret_ctr = 0;
+    for (int x = 0; x < stack_cur_size(s); x++) {
+        if (s.arr[x] != '0' || (!lead_zero && s.arr[x] == '0')) {
+            lead_zero = false;
+            num[ret_ctr++] = s.arr[x];
+        }
+    }
+    num[ret_ctr] = 0;
 
-	int numCtr = 0;
-	for (int x = 0; x < len; x++)
-	{
-		if (tmpNum[x] == 'x' || (numCtr == 0 && tmpNum[x] == '0'))
-		{
-			continue;
-		}
-		num[numCtr] = tmpNum[x];
-		numCtr++;
-	}
-	num[numCtr] = 0;
-
-	free(tmpNum);
-	return (numCtr == 0 ? "0" : num); 
+    stack_dinit(s);
+    return (lead_zero ? "0" : num);
 }
-
-
