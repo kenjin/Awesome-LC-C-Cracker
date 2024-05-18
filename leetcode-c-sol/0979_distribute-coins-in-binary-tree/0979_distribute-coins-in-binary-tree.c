b2dc0d759bd232b7f5a44fb30cc97c5e5e0c072d
Ken Jin <kenjcfar@gmail.com>
2024-05-18 09:02:53 +0800

    0979[M]: Rewrite the recursive solution

--- leetcode-c-sol/0979_distribute-coins-in-binary-tree/0979_distribute-coins-in-binary-tree.c
+++ leetcode-c-sol/0979_distribute-coins-in-binary-tree/0979_distribute-coins-in-binary-tree.c
@@ -6,25 +6,30 @@
  *     struct TreeNode *right;
  * };
  */
+int dis_handler(struct TreeNode* root, int* moves) {
+    if (!root)
+        return 0;
 
-int distributeCoinsHelper(struct TreeNode* root, int *ret)
-{
-	if (root == NULL)
-	{
-		return 0;
-	}
+    int l_moves = dis_handler(root->left, moves);
+    int r_moves = dis_handler(root->right, moves);
 
-	int tmp = distributeCoinsHelper(root->left, ret) + distributeCoinsHelper(root->right, ret) + root->val;
-	*ret += abs(1-tmp);
-	root->val = 1;
-	return tmp-1;
+    // Add total moves needed from left/right sub-tree
+    root->val += l_moves;
+    root->val += r_moves;
+    *moves += abs(l_moves);
+    *moves += abs(r_moves);
+    // - Return the total moves needed for the sub-tree, minus 1 for itself.
+    // - If the return value is positive, it means the sub-tree has extra coins
+    //   to return.
+    // - If the return value is negative, it means the sub-tree needs coins to
+    //   be returned. 
+    // - If the return value is zero, it means the sub-tree. does NOT need any 
+    //   moves for coins.
+    return root->val - 1;
 }
 
-
-int distributeCoins(struct TreeNode* root)
-{
-	int ret = 0;
-	distributeCoinsHelper(root, &ret);    
-	return ret;
+int distributeCoins(struct TreeNode* root) {
+    int moves = 0;
+    dis_handler(root, &moves);
+    return moves;
 }
-
