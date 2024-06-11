659872ff81a9ed8f286a39147e036861413bde39
Ken Jin <kenjcfar@gmail.com>
2024-06-11 14:27:43 +0800

    1047[E]: Rewrite stack solution

--- leetcode-c-sol/1047_remove-all-adjacent-duplicates-in-string/1047_remove-all-adjacent-duplicates-in-string.c
+++ leetcode-c-sol/1047_remove-all-adjacent-duplicates-in-string/1047_remove-all-adjacent-duplicates-in-string.c
@@ -1,84 +1,18 @@
+char* removeDuplicates(char* s) {
+    int slen = strlen(s);
+    char* stack = malloc(sizeof(char) * slen);
+    int stack_ctr = 0;
+    for (int i = 0; i < slen; i++) {
+        if (stack_ctr == 0 || stack[stack_ctr - 1] != s[i])
+            stack[stack_ctr++] = s[i];
+        else
+            stack_ctr--;
+    }
 
-typedef struct
-{
-	int size;
-	int cur;
-	char *arr;
-} STACK;
+    for (int i = 0; i < stack_ctr; i++)
+        s[i] = stack[i];
 
-STACK* stackCreate(int size) 
-{
-	STACK *obj = malloc(sizeof(STACK));
-	obj->size = size;
-	obj->cur = -1;
-	obj->arr = malloc(sizeof(char)*size);
-	return obj;
+    s[stack_ctr] = 0;
+    free(stack);
+    return s;
 }
-
-int stackGetSize(STACK* obj)
-{
-	return obj->cur + 1;
-}
-
-bool stackIsEmpty(STACK *obj)
-{
-	return (obj->cur == -1 ? true : false);
-}
-
-void stackPush(STACK* obj, char c)
-{
-	obj->cur += 1;
-	obj->arr[obj->cur] = c;
-}
-
-char stackPop(STACK* obj)
-{
-	if (stackIsEmpty(obj))
-	{
-		printf("WARNING: empty stack!\n");
-		return;
-	}
-
-	obj->cur -= 1;
-	return obj->arr[obj->cur+1];
-}
-
-char stackTop(STACK* obj)
-{
-	if (stackIsEmpty(obj))
-	{
-		return -1;
-	}
-	return obj->arr[obj->cur];
-}
-
-void stackFree(STACK* obj) 
-{
-	free(obj->arr);
-	free(obj);
-}
-
-char * removeDuplicates(char * S)
-{
-	int len = strlen(S);
-	STACK *st = stackCreate(len);
-	for (int i = 0; i < len; i++)
-	{
-		if (!stackIsEmpty(st) && S[i] == stackTop(st))
-		{
-			stackPop(st);
-			continue;
-		} 
-		stackPush(st, S[i]);
-	}
-
-	int size = stackGetSize(st);
-	S[size] = 0;
-	for (int i = size-1; i >= 0; i--)
-	{
-		S[i] = stackPop(st);
-	}
-	stackFree(st);
-	return S;
-}
-
